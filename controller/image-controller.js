322d71e4467598cfad721f8241fe18f2817e5587
Dishant Balotra <117922857+Dishant4277@users.noreply.github.com>
2023-08-06 01:51:22 +0530

    Update image-controller.js

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from "gridfs-stream";
 import mongoose from "mongoose";
 
-const url = "http://localhost:8000";
+const url = "https://serverbackend-2uta.onrender.com";
 
 let gfs, gridfsBucket;
 const conn = mongoose.connection;
